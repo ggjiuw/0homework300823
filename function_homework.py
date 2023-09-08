1d2e16489b17b23864af129ff5cbaf2fbe953f93
ggjiuw <herezoom1231@gmail.com>
2023-09-08 18:10:54 +0300

    add some code in 8 line

--- function_homework.py
+++ function_homework.py
@@ -4,8 +4,8 @@ def filter_string(input_string) -> list:
     count = 0
     symbol = 0
     while symbol < len(input_string) and count < 100:
-        char = input_string[symbol].lower()
-        if char != 'm' and char != 'n':
+        char = input_string[symbol]
+        if char != 'm' and char != 'n' and char != 'M' and char != 'N':
             result.append(input_string[symbol])
             count += 1
         symbol += 1
