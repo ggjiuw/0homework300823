ba3da1ee5c745cfb9ca596ca08a57514f355c5d6
ggjiuw <herezoom1231@gmail.com>
2023-09-08 10:42:50 +0300

    test for homework

---
+++ test_function_homework.py
@@ -0,0 +1,14 @@
+from function_homework import filter_string
+
+def test_n_filter():
+    expected_result = ['1', '2', '3', ' ', ' ', 'a', 'b', ' ']
+    input_string = 'n123 n ab n'
+    actual_result = filter_string(input_string)
+    assert expected_result == actual_result
+
+
+def test_m_filter():
+    expected_result = ['1', '2', '3', ' ', ' ', 'a', 'b', ' ']
+    input_string = 'm123 m ab m'
+    actual_result = filter_string(input_string)
+    assert expected_result == actual_result
